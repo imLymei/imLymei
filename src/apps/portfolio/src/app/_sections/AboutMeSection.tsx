1da67f3849bfd4918dc2d2ca46d8911529bce8a7
imlymei <lymeicontato@gmail.com>
2026-03-16 20:59:29 -0300

    feat: complete about me section

--- src/apps/portfolio/src/app/_sections/AboutMeSection.tsx
+++ src/apps/portfolio/src/app/_sections/AboutMeSection.tsx
@@ -1,7 +1,8 @@
 import Page from "@/components/Page";
 import Patch from "@/components/ui/Patch";
-import { TECH_STACK } from "@/lib/constants";
+import { ACADEMICS, EXPERIENCES, TECH_STACK } from "@/lib/constants";
 import { getUniqueHashColor } from "@/lib/utils";
+import ExperienceComponent from "./aboutMe/ExperienceComponent";
 
 export default function AboutMeSection() {
   return (
@@ -9,8 +10,8 @@ export default function AboutMeSection() {
       <h2 className="text-center text-4xl font-bold md:text-left md:text-6xl">
         Sobre Mim
       </h2>
-      <div className="flex flex-col md:flex-row gap-12">
-        <div className="flex-1 flex flex-col gap-12">
+      <div className="flex flex-col gap-12 lg:flex-row">
+        <div className="flex flex-1 flex-col gap-12">
           <div className="flex flex-col gap-2">
             <p>
               Oi, sou Felipe Cardoso, engenheiro de software morando em{" "}
@@ -45,33 +46,25 @@ export default function AboutMeSection() {
             </p>
           </div>
           <div className="flex flex-col gap-6">
-            <h3 className="font-bold text-xl text-center md:text-left md:text-2xl">
+            <h3 className="text-center text-xl font-bold md:text-left md:text-2xl">
               Experiências
             </h3>
-            <ul className="flex flex-col gap-2">
-              <li>
-                <p className="font-bold">
-                  Trabalho 1{" "}
-                  <span className="text-neutral-500">
-                    (Janeiro 2024 - Dezembro 2025)
-                  </span>
-                </p>
-                <p className="md:pl-4">Sobre o Trabalho 1</p>
-              </li>
-              <li>
-                <p className="font-bold">Trabalho 2</p>
-                <p className="md:pl-4">Sobre o Trabalho 2</p>
-              </li>
-              <li>
-                <p className="font-bold">Trabalho 3</p>
-                <p className="md:pl-4">Sobre o Trabalho 3</p>
-              </li>
+            <ul className="flex flex-col gap-4">
+              {EXPERIENCES.map((experience) => (
+                <ExperienceComponent
+                  key={`experience-${experience.role}`}
+                  role={experience.role}
+                  employer={experience.employer}
+                  description={experience.description}
+                  period={experience.period}
+                />
+              ))}
             </ul>
           </div>
         </div>
-        <div>
+        <div className="flex flex-col-reverse gap-12 lg:flex-col">
           <div className="flex flex-col gap-6">
-            <h3 className="font-bold text-xl text-center md:text-left md:text-2xl">
+            <h3 className="text-center text-xl font-bold md:text-left md:text-2xl">
               Tech Stack
             </h3>
             <ul className="flex flex-col gap-2">
@@ -83,6 +76,25 @@ export default function AboutMeSection() {
                   >
                     {tech}
                   </Patch>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex flex-col gap-6">
+            <h3 className="text-center text-xl font-bold md:text-left md:text-2xl">
+              Formação Acadêmica
+            </h3>
+            <ul className="flex flex-col gap-2">
+              {ACADEMICS.map((academic) => (
+                <li
+                  key={`academic-${academic.university}-${academic.areaOfStudy}`}
+                >
+                  <p className="text-lg font-bold">{academic.university}</p>
+                  <div className="flex flex-col">
+                    <p className="text-violet-500">{academic.degree}</p>
+                    <p>{academic.areaOfStudy}</p>
+                    <p className="text-neutral-500">{academic.period}</p>
+                  </div>
                 </li>
               ))}
             </ul>
--- src/apps/portfolio/src/components/ui/Patch.tsx
+++ src/apps/portfolio/src/components/ui/Patch.tsx
@@ -4,8 +4,8 @@ type PathProps = React.HTMLAttributes<HTMLDivElement>;
 
 export default function Patch({ children, className, ...props }: PathProps) {
   return (
-    <div className={cx("p-1 rounded-full", className)} {...props}>
-      <div className="py-1 lg:py-2 px-4 border-2 border-dashed border-neutral-50/50  rounded-full">
+    <div className={cx("rounded-full p-1", className)} {...props}>
+      <div className="rounded-full border-2 border-dashed border-neutral-50/50 px-4 py-1 lg:py-2">
         {children}
       </div>
     </div>
