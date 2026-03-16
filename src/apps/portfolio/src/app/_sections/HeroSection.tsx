9d9b6c775db002f354d6a35971f454dd04da2de6
imlymei <lymeicontato@gmail.com>
2026-03-16 12:32:57 -0300

    chore: update HeroSection

--- src/apps/portfolio/src/app/_sections/HeroSection.tsx
+++ src/apps/portfolio/src/app/_sections/HeroSection.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Button from "@/components/ui/Button";
+import Patch from "@/components/ui/Patch";
 import useStatusHook from "@/hooks/useStatusHook";
 import { APP } from "@/lib/config";
 import { ICON_SIZES, ICONS } from "@/lib/constants";
@@ -49,15 +50,19 @@ export default function HeroSection() {
                 <path d="M150 0v28.815a11.936 11.936 0 0 0 23.642 2.328l5.622-28.262-5.622 28.262a11.936 11.936 0 0 0 22.734 6.896l11.027-26.622-11.027 26.622a11.936 11.936 0 0 0 20.952 11.199l16.009-23.959-16.009 23.959a11.936 11.936 0 0 0 18.364 15.07l20.375-20.375-20.375 20.375a11.936 11.936 0 0 0 15.07 18.364l23.959-16.009-23.959 16.009a11.936 11.936 0 0 0 11.199 20.952l26.622-11.027-26.622 11.027a11.937 11.937 0 0 0 6.896 22.734l28.262-5.622-28.262 5.622A11.937 11.937 0 0 0 271.185 150H300h-28.815a11.936 11.936 0 0 0-2.328 23.642l28.262 5.622-28.262-5.622a11.936 11.936 0 0 0-6.896 22.734l26.622 11.027-26.622-11.027a11.936 11.936 0 0 0-11.199 20.952l23.959 16.009-23.959-16.009a11.936 11.936 0 0 0-15.07 18.364l20.375 20.375-20.375-20.375a11.936 11.936 0 0 0-18.364 15.07l16.009 23.959-16.009-23.959a11.936 11.936 0 0 0-20.952 11.199l11.027 26.622-11.027-26.622a11.937 11.937 0 0 0-22.734 6.896l5.622 28.262-5.622-28.262A11.937 11.937 0 0 0 150 271.185V300v-28.815a11.936 11.936 0 0 0-23.642-2.328l-5.622 28.262 5.622-28.262a11.936 11.936 0 0 0-22.734-6.896l-11.027 26.622 11.027-26.622a11.936 11.936 0 0 0-20.952-11.199l-16.009 23.959 16.009-23.959a11.936 11.936 0 0 0-18.364-15.07l-20.375 20.375 20.375-20.375a11.936 11.936 0 0 0-15.07-18.364l-23.959 16.009 23.959-16.009a11.936 11.936 0 0 0-11.199-20.952l-26.622 11.027 26.622-11.027a11.937 11.937 0 0 0-6.896-22.734l-28.262 5.622 28.262-5.622A11.937 11.937 0 0 0 28.815 150H0h28.815a11.936 11.936 0 0 0 2.328-23.642l-28.262-5.622 28.262 5.622a11.936 11.936 0 0 0 6.896-22.734L11.417 92.597l26.622 11.027a11.936 11.936 0 0 0 11.199-20.952L25.279 66.663l23.959 16.009a11.936 11.936 0 0 0 15.07-18.364L43.933 43.933l20.375 20.375a11.936 11.936 0 0 0 18.364-15.07L66.663 25.279l16.009 23.959a11.936 11.936 0 0 0 20.952-11.199L92.597 11.417l11.027 26.622a11.937 11.937 0 0 0 22.734-6.896l-5.622-28.262 5.622 28.262A11.937 11.937 0 0 0 150 28.815V0Z" />
               </svg>
             </div>
-            <div className="absolute right-6 -bottom-2 flex flex-col gap-1 text-xs max-lg:translate-y-1/2 max-md:translate-x-1/2 lg:text-sm">
-              <p className="flex items-center justify-center rounded-full bg-violet-500 px-4 py-1 lg:py-2">
-                <FaPlus size={ICON_SIZES.SMALL} />
-                UI Designer
-              </p>
-              <p className="flex items-center justify-center rounded-full bg-violet-500 px-4 py-1 lg:py-2">
-                <FaPlus size={ICON_SIZES.SMALL} />
-                UX Designer
-              </p>
+            <div className="absolute -rotate-10 right-6 -bottom-6 flex flex-col gap-1 text-xs max-lg:translate-y-1/2 max-md:translate-x-1/2 lg:text-sm">
+              <Patch className="bg-blue-500">
+                <p className="flex gap-2 items-center">
+                  <FaPlus size={ICON_SIZES.SMALL} />
+                  UI Designer
+                </p>
+              </Patch>
+              <Patch className="bg-violet-500">
+                <p className="flex gap-2 items-center">
+                  <FaPlus size={ICON_SIZES.SMALL} />
+                  UX Designer
+                </p>
+              </Patch>
             </div>
           </span>
         </h1>
@@ -84,7 +89,7 @@ export default function HeroSection() {
         </Button>
       </div>
       <a
-        href="#projects"
+        href="#about-me"
         className="absolute bottom-6 flex items-center gap-2 hover:text-violet-500"
       >
         Saiba mais{" "}
